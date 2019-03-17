e8e389c94e56c79d64441d3d61b7b36cd12753ed
snail_88@126.com <xing947950356>
2019-03-17 21:41:46 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Button from 'antd/lib/button';
 import './App.css';
-
+import Login from './page/Login/Login'
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <h1 style={{marginBottom:'1em'}}>秀阁潮店后台管理系统</h1>
+                <Login/>
+            </div>
+        );
+    }
 }
 
 export default App;
