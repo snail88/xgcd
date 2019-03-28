fa6e00be1b62309dabc2c230c3a884c01d3520fc
snail_88@126.com <xing947950356>
2019-03-28 23:42:26 +0800

    webpack配置,样式修改

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import MRoute from './routers/index';
+import 'antd/dist/antd.css';
 
 import registerServiceWorker from './registerServiceWorker';
 
--- src/page/common/Login.js
+++ src/page/common/Login.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import '../../style/login.less';
+import axios from 'axios';
 import { Form, Icon, Input, Button, Checkbox, message, Spin } from 'antd';
+import config from '../../utils/config'
 const FormItem = Form.Item;
 
 const login = [{
@@ -25,10 +27,33 @@ function PatchUser(values) {  //匹配用户
 class NormalLoginForm extends Component {
     state = {
         isLoding:false,
+        data:{}
     };
     handleSubmit = (e) => {
         e.preventDefault();
         this.props.form.validateFields((err, values) => {
+            axios.get('http://149.129.117.122:8088/xg/member/list')
+                .then(function (response) {
+                    console.log(response);
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+            // axios({
+            //     method: 'post',
+            //     url: `${config.BASE_URL}/user/login`,
+            //     headers:{'Content-Type': 'application/json'},
+            //     data:{
+            //         userName: values.username,
+            //         password: values.password
+            //     }
+            // })
+            //     .then(function (response) {
+            //         console.log(response);
+            //     })
+            //     .catch(function (error) {
+            //         console.log(error);
+            //     });
             if (!err) {
                 console.log('Received values of form: ', values);
                 if(PatchUser(values)){
@@ -37,14 +62,14 @@ class NormalLoginForm extends Component {
                     });
 
                     localStorage.setItem('mspa_user',JSON.stringify(values));
-                    message.success('login successed!'); //成功信息
+                    message.success('登录成功!'); //成功信息
                     let that = this;
                     setTimeout(function() { //延迟进入
                         that.props.history.push({pathname:'/app',state:values});
                     }, 2000);
 
                 }else{
-                    message.error('login failed!'); //失败信息
+                    message.error('账号密码错误!'); //失败信息
                 }
             }
         });
@@ -57,7 +82,7 @@ class NormalLoginForm extends Component {
             <div className="login">
                 <div className="login-form">
                     <div className="login-logo">
-                        <div className="login-name">MSPA</div>
+                        <div className="login-name">秀阁潮店后台管理系统</div>
                     </div>
                     <Form onSubmit={this.handleSubmit} style={{maxWidth: '300px'}}>
                         <FormItem>
@@ -77,18 +102,18 @@ class NormalLoginForm extends Component {
                         <FormItem style={{marginBottom:'0'}}>
                             {getFieldDecorator('remember', {
                                 valuePropName: 'checked',
-                                initialValue: true,
+                                initialValue: false,
                             })(
                                 <Checkbox>记住我</Checkbox>
                             )}
-                            <a className="login-form-forgot" href="" style={{float:'right'}}>忘记密码?</a>
+                            {/*<a className="login-form-forgot" href="" style={{float:'right'}}>忘记密码?</a>*/}
                             <Button type="primary" htmlType="submit" className="login-form-button" style={{width: '100%'}}>
                                 登录
                             </Button>
-                            Or <a href="">现在就去注册!</a>
+                            {/*Or <a href="">现在就去注册!</a>*/}
                         </FormItem>
                     </Form>
-                    <a className="githubUrl" href="https://github.com/zhaoyu69/antd-spa"> </a>
+                    {/*<a className="githubUrl" href="https://github.com/zhaoyu69/antd-spa"> </a>*/}
                 </div>
             </div>
         );
