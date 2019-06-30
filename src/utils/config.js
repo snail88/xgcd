980ca7b5ea0d8da44778943de44df8ac972be5e7
snail_88@126.com <snail_88@126.com>
2019-06-30 15:37:29 +0800

    jenkins 自动打包部署测试3

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    BASE_URL:'http://localhost:8088/xg'
-    // BASE_URL:'http://149.129.117.122:8088/xg'
+    // BASE_URL:'http://localhost:8088/xg'
+    BASE_URL:'http://149.129.117.122:8088/xg'
 }
